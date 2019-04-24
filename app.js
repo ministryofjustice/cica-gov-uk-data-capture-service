134369fa3385237d54f0221759629385fbaca160
Stephen Sinclair <Stephen.Sinclair@CICA.GOV.UK>
2019-04-24 12:46:51 +0100

    Add JWT support

--- app.js
+++ app.js
@@ -1,22 +1,84 @@
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const helmet = require('helmet');
-
 const questionnaireRouter = require('./questionnaire/routes');
-const app = express();
 
+const app = express();
 
 app.use(helmet());
 app.use(logger('dev'));
 // https://expressjs.com/en/api.html#express.json
-app.use(express.json());
+app.use(express.json({type: 'application/vnd.api+json'}));
 // https://expressjs.com/en/api.html#express.urlencoded
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
-
+// Default to JSON:API content type for all responses
+app.use((req, res, next) => {
+    res.type('application/vnd.api+json');
+    next();
+});
 
 app.use('/questionnaires', questionnaireRouter);
+
+// Express doesn't treat 404s as errors. If the following handler has been reached then nothing else matched e.g. a 404
+// https://expressjs.com/en/starter/faq.html#how-do-i-handle-404-responses
+app.use(req => {
+    const err = Error(`Endpoint ${req.url} does not exist`);
+    err.name = 'HTTPError';
+    err.statusCode = 404;
+    err.error = '404 Not Found';
+    throw err;
+});
+
+// Central error handler
+// https://www.joyent.com/node-js/production/design/errors
+// https://github.com/i0natan/nodebestpractices/blob/master/sections/errorhandling/centralizedhandling.md
+app.use(async (err, req, res, next) => {
+    const error = {errors: []};
+
+    if (err.statusCode === 404) {
+        error.errors.push({
+            status: 404,
+            title: err.error,
+            detail: err.message
+        });
+
+        return res.status(404).json(error);
+    }
+
+    if (err.statusCode === 403) {
+        error.errors.push({
+            status: 403,
+            title: err.error,
+            detail: err.message
+        });
+
+        return res.status(403).json(error);
+    }
+
+    if (err.statusCode === 400) {
+        error.errors.push({
+            status: 400,
+            title: err.error,
+            detail: err.message
+        });
+
+        return res.status(400).json(error);
+    }
+
+    if (err.name === 'UnauthorizedError') {
+        error.errors.push({
+            status: 401,
+            title: err.error,
+            detail: err.message
+        });
+
+        return res.status(401).json(error);
+    }
+
+    // Non-operational error
+    return next(err);
+});
 
 module.exports = app;
--- questionnaire/routes.js
+++ questionnaire/routes.js
@@ -1,9 +1,21 @@
 const express = require('express');
-
+const validateJWT = require('express-jwt');
+const jwtAuthz = require('express-jwt-authz');
 const q = require('./questionnaire');
-const router = express.Router();
 
-router.route('/:questionnaireId/sections/answers').get((req, res) => {
+const router = express.Router();
+const rxCaseReference = /^[0-9]{2}\\[0-9]{6}$/;
+
+// express-jwt-authz doesnt allow the "failWithError" opt to be set globally,
+// wrap it to avoid passing it in manually on every call
+function permissions(...scopes) {
+    return jwtAuthz(scopes, {failWithError: true});
+}
+
+// Ensure JWT is valid
+router.use(validateJWT({secret: process.env.SECRET}));
+
+router.route('/:questionnaireId/sections/answers').get(permissions('read:answers'), (req, res) => {
     // /questionnaires/68653be7-877f-4106-b91e-4ba8dac883f3/sections/answers
 
     const response = {
@@ -14,7 +26,33 @@ router.route('/:questionnaireId/sections/answers').get((req, res) => {
         }
     };
 
-    res.type('application/vnd.api+json');
+    res.json(response);
+});
+
+router.route('/:questionnaireId').patch(permissions('update:questionnaires'), (req, res) => {
+    if (
+        req.body.data &&
+        req.body.data.attributes &&
+        req.body.data.attributes.caseReference &&
+        rxCaseReference.test(req.body.data.attributes.caseReference)
+    ) {
+        q.caseReference = req.body.data.attributes.caseReference;
+    } else {
+        const err = Error(`Bad request`);
+        err.name = 'HTTPError';
+        err.statusCode = 400;
+        err.error = '400 Bad Request';
+        throw err;
+    }
+
+    const response = {
+        data: {
+            type: 'questionnaires',
+            id: '68653be7-877f-4106-b91e-4ba8dac883f3',
+            attributes: q
+        }
+    };
+
     res.json(response);
 });
 
