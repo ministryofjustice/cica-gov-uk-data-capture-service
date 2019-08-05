53eefc50ed4e3b795fb10605def64dba8fdb85a6
armoj <adrian.roworth@cica.gov.uk>
2019-08-05 10:52:33 +0100

    Get submission endpoint
    * Add Message Bus service
    
    * Add GET submissions endpoint to OpenAPI definition
    
    * Add GET submissions controller and logic
    
    * Update tests

--- questionnaire/routes.js
+++ questionnaire/routes.js
@@ -107,4 +107,33 @@ router
         }
     });
 
+router
+    .route('/:questionnaireId/submissions')
+    .get(permissions('read:questionnaires'), async (req, res, next) => {
+        try {
+            const {questionnaireId} = req.params;
+            const questionnaireService = createQuestionnaireService({logger: req.log});
+            const submissionStatus = await questionnaireService.getQuestionnaireSubmissionStatus(
+                questionnaireId
+            );
+            // the default value for this column is "NOT_STARTED", so if it doesn't
+            // exist, then it must not be a valid questionnaire ID.
+            if (!submissionStatus) {
+                const err = Error(
+                    `Questionnaire with questionnaireId "${questionnaireId}" does not exist`
+                );
+                err.name = 'HTTPError';
+                err.statusCode = 404;
+                err.error = '404 Not Found';
+                throw err;
+            }
+
+            const response = await questionnaireService.getSubmissionResponseData(questionnaireId);
+
+            res.status(200).json(response);
+        } catch (err) {
+            next(err);
+        }
+    });
+
 module.exports = router;
