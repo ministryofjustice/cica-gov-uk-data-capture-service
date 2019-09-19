354b50c2d12757ed62cc53c4ceffc500dc4016e3
Adrian Roworth <adrian.roworth@CICA.GOV.UK>
2019-09-19 11:48:14 +0100

    Update CSP settings

--- app.js
+++ app.js
@@ -42,7 +42,10 @@ app.use(helmet());
 app.use(
     helmet.contentSecurityPolicy({
         directives: {
-            defaultSrc: ["'self'"]
+            defaultSrc: ["'self'"],
+            scriptSrc: ["'self'", "'unsafe-inline'"],
+            styleSrc: ["'self'", "'unsafe-inline'"],
+            imgSrc: ["'*'", 'data:']
         }
     })
 );
