c048732f0fef43c5d4456b895962a33c1764882f
sinclairs <stephen.sinclair@cica.gov.uk>
2020-04-30 10:19:12 +0100

    Add test coverage threshold to jest config (#109)

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,33 @@ process.env.DCS_JWT_SECRET = '123';
 process.env.DCS_LOG_LEVEL = 'silent';
 process.env.NOTIFY_API_KEY = '123apiKey';
 
+config.coverageThreshold = {
+    './*/!(message-bus|notify|request)/**/*.js': {
+        statements: 54,
+        branches: 60,
+        functions: 50,
+        lines: 56
+    },
+    './*/message-bus/**/*.js': {
+        statements: 15,
+        branches: 60,
+        functions: 0,
+        lines: 15
+    },
+    './*/notify/**/*.js': {
+        statements: 42,
+        branches: 60,
+        functions: 0,
+        lines: 42
+    },
+    './*/request/**/*.js': {
+        statements: 23,
+        branches: 60,
+        functions: 0,
+        lines: 23
+    }
+};
+
 /*! m0-start */
 module.exports = config;
 /*! m0-end */
-
