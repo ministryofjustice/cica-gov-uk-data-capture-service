bc1dd8760b100abf403bd31e27aedf17a82bea17
Stephen S <Stephen.Sinclair@CICA.GOV.UK>
2022-07-19 11:52:24 +0100

    Remove redundant isJsonExpression module. Superseded by q-expression.isJsonExpression

--- questionnaire/questionnaire/utils/isJsonExpression/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-function isJsonExpression(value) {
-    return value && Array.isArray(value) && typeof value[0] === 'string' && value[0][0] === '|';
-}
-
-module.exports = isJsonExpression;
