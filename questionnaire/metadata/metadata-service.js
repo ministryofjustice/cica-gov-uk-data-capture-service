c07fc034cec97f8f6d1a4e80b50d74b811e28c43
Barry Piccinni <barry.piccinni@CICA.GOV.UK>
2023-02-01 18:05:30 +0000

    Extract metadata into own folder

---
+++ questionnaire/metadata/metadata-service.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const defaults = {};
+defaults.createQuestionnaireDAL = require('../questionnaire-dal');
+
+function createMetadataService({
+    logger,
+    createQuestionnaireDAL = defaults.createQuestionnaireDAL
+} = {}) {
+    const db = createQuestionnaireDAL({logger});
+
+    async function getMetadata(query = {}) {
+        const results = await db.getQuestionnaireMetadata(query);
+
+        // Add expiry time & map
+        const meta = results.rows.map(data => ({
+            type: 'metadata',
+            id: data.id,
+            attributes: {
+                'questionnaire-id': data.id,
+                'questionnaire-document-version': data['questionnaire-version'],
+                created: data.created,
+                modified: data.modified,
+                state: data.submission_status,
+                'user-id': data['user-id'],
+                expires: new Date(
+                    new Date(
+                        new Date(data.created).setUTCDate(new Date(data.created).getUTCDate() + 31)
+                    ).setUTCHours(0, 0, 0, 0)
+                ).toISOString()
+            }
+        }));
+
+        return {
+            data: meta
+        };
+    }
+
+    return Object.freeze({
+        getMetadata
+    });
+}
+
+module.exports = createMetadataService;
