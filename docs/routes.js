7199e5c9e1a3860b899e9d7b4a8bd67c8a5a1477
Barry Piccinni <barry.piccinni@CICA.GOV.UK>
2023-05-05 21:35:22 +0100

    Swagger dropdown initial commit

--- docs/routes.js
+++ docs/routes.js
@@ -4,12 +4,32 @@ const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 
 const swaggerDocument = require('../openapi/openapi.json');
+const swaggerV2Document = require('../openapi/openapiV2.json');
 
 const router = express.Router();
 
+const options = {
+    explorer: true,
+    swaggerOptions: {
+        urls: [
+            {
+                spec: swaggerDocument,
+                name: 'v1'
+            },
+            {
+                spec: swaggerV2Document,
+                name: 'v2'
+            }
+        ]
+    }
+};
+
 // Ensure JWT is valid
 // router.use(validateJWT({secret: process.env.SECRET}));
-router.use('/', swaggerUi.serve);
-router.get('/', swaggerUi.setup(swaggerDocument));
+router.use('/v1', swaggerUi.serve);
+router.get('/v1', swaggerUi.setup(null, options));
+
+router.use('/v2', swaggerUi.serve);
+router.get('/v2', swaggerUi.setup(null, options));
 
 module.exports = router;
