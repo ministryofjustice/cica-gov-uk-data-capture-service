0895c4eafbb8719ae6bb402a8ec14cd336c07c6e
Barry Piccinni <barry.piccinni@CICA.GOV.UK>
2023-05-05 17:06:06 +0100

    Add DAL tests for insertOwner

--- questionnaire/questionnaire-dal.test.js
+++ questionnaire/questionnaire-dal.test.js
@@ -4,30 +4,44 @@ const DB_QUERY_ERROR_QUESTIONNAIRE_ID = 'acbfcd8e-1299-478a-a9f1-7005f4b713ed';
 const DB_QUERY_ERROR_SUBMISSION_STATUS_ID = 'FAIL_TEST';
 const DB_QUERY_ROW_COUNT_ZERO_QUESTIONNAIRE_ID = 'c9723b81-c50d-4050-805f-108995067913';
 const DB_QUERY_ROW_COUNT_ZERO_SUBMISSION_STATUS = 'ZERO_ROWS_SUBMISSION_STATUS';
+const DB_QUERY_SUCCESS_QUESTIONNAIRE_ID = '12345678-c50d-4050-805f-108995067913';
 
-jest.doMock('../db/index.js', () => () => ({
-    query: (query, parameters) => {
-        if (parameters.includes(DB_QUERY_ERROR_QUESTIONNAIRE_ID)) {
-            throw new Error('DB_QUERY_ERROR');
-        }
+beforeEach(() => {
+    jest.clearAllMocks();
+    jest.resetModules();
+});
 
-        if (parameters.includes(DB_QUERY_ERROR_SUBMISSION_STATUS_ID)) {
-            throw new Error('DB_QUERY_ERROR');
-        }
+jest.doMock('../db/index.js', () => {
+    const dbServiceMock = {
+        query: jest.fn((query, parameters) => {
+            if (parameters.includes(DB_QUERY_ERROR_QUESTIONNAIRE_ID)) {
+                throw new Error('DB_QUERY_ERROR');
+            }
 
-        if (parameters.includes(DB_QUERY_ROW_COUNT_ZERO_QUESTIONNAIRE_ID)) {
+            if (parameters.includes(DB_QUERY_ERROR_SUBMISSION_STATUS_ID)) {
+                throw new Error('DB_QUERY_ERROR');
+            }
+
+            if (parameters.includes(DB_QUERY_ROW_COUNT_ZERO_QUESTIONNAIRE_ID)) {
+                return {
+                    rows: [],
+                    rowCount: 0
+                };
+            }
+
             return {
-                rows: [],
-                rowCount: 0
+                rows: [{status: 'ok'}],
+                rowCount: 1
             };
-        }
+        })
+    };
 
-        return {
-            rows: [],
-            rowCount: 0
-        };
-    }
-}));
+    return () => dbServiceMock;
+});
+
+const mockedDbService = require('../db/index.js')({
+    logger: () => 'Logged from DAL test'
+});
 
 const createQuestionnaireDAL = require('./questionnaire-dal');
 
@@ -178,6 +192,48 @@ describe('questionnaire data access layer', () => {
             await expect(
                 questionnaireDAL.updateQuestionnaireModifiedDate(DB_QUERY_ERROR_QUESTIONNAIRE_ID)
             ).rejects.toThrow('DB_QUERY_ERROR');
+        });
+    });
+
+    describe('insertOwner', () => {
+        const query =
+            "UPDATE questionnaire SET questionnaire = jsonb_set(questionnaire,  '{answers}' , questionnaire -> 'answers' || json_build_object('owner', json_build_object('owner-id', $2::text, 'isAuthenticated', $3::boolean))::jsonb, true) WHERE id = $1";
+        it('Should run an update questionnaire query to update the owner value', async () => {
+            const questionnaireId = DB_QUERY_SUCCESS_QUESTIONNAIRE_ID;
+            const owner = {
+                id: 'urn:uuid:f81d4fae-7dec-11d0-a765-00a0c91e6bf6',
+                isAuthenticated: true
+            };
+            const questionnaireDAL = createQuestionnaireDAL({logger: jest.fn(), owner});
+            await questionnaireDAL.insertOwner(questionnaireId);
+
+            expect(mockedDbService.query).toHaveBeenCalledTimes(1);
+            expect(mockedDbService.query).toHaveBeenCalledWith(query, [
+                questionnaireId,
+                owner.id,
+                owner.isAuthenticated
+            ]);
+        });
+
+        it('Should error gracefully if no rows are updated', async () => {
+            const questionnaireId = DB_QUERY_ROW_COUNT_ZERO_QUESTIONNAIRE_ID;
+            const owner = {
+                id: 'urn:uuid:f81d4fae-7dec-11d0-a765-00a0c91e6bf6',
+                isAuthenticated: true
+            };
+            const questionnaireDAL = createQuestionnaireDAL({logger: jest.fn(), owner});
+
+            await expect(questionnaireDAL.insertOwner(questionnaireId)).rejects.toThrow(
+                `Questionnaire "${DB_QUERY_ROW_COUNT_ZERO_QUESTIONNAIRE_ID}" owner was not updated successfully`
+            );
+        });
+
+        it('Should handle errors gracefully', async () => {
+            const questionnaireId = DB_QUERY_SUCCESS_QUESTIONNAIRE_ID;
+            const owner = undefined;
+            const questionnaireDAL = createQuestionnaireDAL({logger: jest.fn(), owner});
+
+            await expect(questionnaireDAL.insertOwner(questionnaireId)).rejects.toThrow();
         });
     });
 });
