f965ec18971df7ef1f618f56934bf93d61181754
L-Storm <lloyd.storm@informed.com>
2023-05-18 11:09:21 +0100

    adition of identify-questionare

---
+++ services/identify-questionare/identify-questionare.js
@@ -0,0 +1,12 @@
+
+
+function createIdnetifyQuestionareService() {
+    
+}
+// read questionare jsonb, identify case type. 
+
+//read previous ID for relevent case from DB.
+
+// generate new ID based on previous case number. 
+
+//Do with ID (print for now)
---
+++ services/identify-questionare/identify-questionare.test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// requirements: currently unknown. 
+
+describe('identification-id-exists', () => {
+    it('should ensure that when a new form is submitted to the DCS, a tariffID is generated.', () => {
+        const content = 'foo ||/foo/bar|| baz';
+        // const result = replaceJsonPointer(content, {
+        //     foo: {
+        //         bar: 'bar'
+        //     }
+
+        //     expect(result).toEqual('foo bar baz');
+        // });
+
+        
