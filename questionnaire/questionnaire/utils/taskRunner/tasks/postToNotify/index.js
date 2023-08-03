9c231bc3a7cd6ba2d6255fd778b08d5600127877
msenkiw <michael.senkiw@informed.com>
2023-08-03 17:27:05 +0100

    [CDBT-181] Notify task

---
+++ questionnaire/questionnaire/utils/taskRunner/tasks/postToNotify/index.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const VError = require('verror');
+const createSqsService = require('../../../../../../services/sqs/index');
+const createQuestionnaireHelper = require('../../../../questionnaire');
+
+async function sendNotifyMessageToSQS({questionnaire, logger}) {
+    const questionnaireId = questionnaire.id;
+    logger.info(`Sending notify message to SQS for questionnaire with id ${questionnaireId}`);
+    const sqsService = createSqsService({logger});
+    const questionnaireDef = createQuestionnaireHelper({questionnaireDefinition: questionnaire});
+    const permittedActions = questionnaireDef.getPermittedActions();
+    let sqsResponse = {MessageId: 'MessageId'};
+
+    await permittedActions.map(async action => {
+        if (action) {
+            if (action.type === 'sendEmail') {
+                const payload = {
+                    templateId: action.data.templateId,
+                    emailAddress: action.data.emailAddress,
+                    personalisation: {
+                        caseReference: action.data.personalisation?.caseReference,
+                        content: action.data.personalisation?.content
+                    },
+                    reference: null
+                };
+                sqsResponse = await sqsService.send(payload, process.env.DCS_NOTIFY_SQS_URL);
+                logger.info(
+                    `Email sent to Notify SQS for questionnaire with id ${questionnaireId}`
+                );
+            }
+
+            if (action.type === 'sendSms') {
+                const payload = {
+                    templateId: action.data.templateId,
+                    phoneNumber: action.data.phoneNumber,
+                    personalisation: {
+                        caseReference: action.data.personalisation?.caseReference,
+                        content: action.data.personalisation?.content
+                    },
+                    reference: null
+                };
+                sqsResponse = await sqsService.send(payload, process.env.DCS_NOTIFY_SQS_URL);
+                logger.info(`SMS sent to Notify SQS for questionnaire with id ${questionnaireId}`);
+            }
+        }
+    });
+
+    if (!sqsResponse || !sqsResponse.MessageId) {
+        logger.error(
+            `Message not sent to Notify SQS for questionnaire with id ${questionnaireId} with error ${sqsResponse}`
+        );
+        throw new VError(
+            {
+                name: 'SendMessageFailed'
+            },
+            `Message not sent to Notify SQS for questionnaire with id ${questionnaireId}`
+        );
+    }
+    return sqsResponse;
+}
+
+module.exports = sendNotifyMessageToSQS;
---
+++ questionnaire/questionnaire/utils/taskRunner/tasks/postToNotify/index.test.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const VError = require('verror');
+const questionnaireWithEmail = require('../test-fixtures/questionnaireCompleteForCheckYourAnswers');
+const questionnaireWithSMS = require('../test-fixtures/questionnaireCompleteWithSMS.json');
+const mockSqsService = require('../../../../../../services/sqs');
+const sendNotifyMessageToSQS = require('.');
+
+const mockLogger = {
+    info: jest.fn(),
+    error: jest.fn()
+};
+
+const mockSendSqsResponse = require('../../../../../test-fixtures/res/post_submissionQueue.json');
+
+jest.mock('../../../../../../services/sqs');
+describe('Post to Notify task', () => {
+    let sendMock;
+    beforeEach(() => {
+        jest.resetModules();
+        jest.resetAllMocks();
+    });
+    it('Should send an email if q-applicant-confirmation-method is email', async () => {
+        sendMock = mockSqsService.mockImplementation(() => ({
+            send: () => mockSendSqsResponse
+        }));
+        const data = {
+            questionnaire: questionnaireWithEmail,
+            logger: mockLogger
+        };
+        const messageResult = await sendNotifyMessageToSQS(data);
+        expect(sendMock).toHaveBeenCalledTimes(1);
+        expect(messageResult).toEqual({
+            MessageId: '741d63a9-8883-4d3b-a6d1-5b93d4709854',
+            body: 'Message sent'
+        });
+    });
+
+    it('Should send an SMS if q-applicant-confirmation-method is SMS', async () => {
+        sendMock = mockSqsService.mockImplementation(() => ({
+            send: () => mockSendSqsResponse
+        }));
+        const data = {
+            questionnaire: questionnaireWithSMS,
+            logger: mockLogger
+        };
+        const messageResult = await sendNotifyMessageToSQS(data);
+        expect(sendMock).toHaveBeenCalledTimes(1);
+        expect(messageResult).toEqual({
+            MessageId: '741d63a9-8883-4d3b-a6d1-5b93d4709854',
+            body: 'Message sent'
+        });
+    });
+
+    it('Should send an throw an error if the message fails to send', async () => {
+        sendMock = mockSqsService.mockImplementation(() => ({
+            send: () => {
+                'Message failed to send';
+            }
+        }));
+        const data = {
+            questionnaire: questionnaireWithSMS,
+            logger: mockLogger
+        };
+
+        await expect(sendNotifyMessageToSQS(data)).rejects.toThrow(VError);
+        expect(sendMock).toHaveBeenCalledTimes(1);
+    });
+});
