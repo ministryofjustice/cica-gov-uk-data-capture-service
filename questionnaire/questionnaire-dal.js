b2f7450a19574a714ae3a15e00e726de41bf8322
Barry Piccinni <barry.piccinni@CICA.GOV.UK>
2023-08-14 17:19:22 +0100

    Update the DAL function to return the questionnaire version

--- questionnaire/questionnaire-dal.js
+++ questionnaire/questionnaire-dal.js
@@ -276,7 +276,7 @@ function questionnaireDAL(spec) {
         let result;
         try {
             result = await db.query(
-                `SELECT id, created, modified, expires, submission_status FROM questionnaire WHERE questionnaire -> 'answers' -> 'owner' ->> 'owner-id' = $1`,
+                `SELECT id, created, modified, expires, submission_status, questionnaire ->> 'version' AS "questionnaire_version" FROM questionnaire WHERE questionnaire -> 'answers' -> 'owner' ->> 'owner-id' = $1`,
                 [ownerId]
             );
         } catch (err) {
@@ -290,7 +290,7 @@ function questionnaireDAL(spec) {
         let result;
         try {
             result = await db.query(
-                `SELECT id, created, modified, expires, submission_status FROM questionnaire WHERE id = $1 AND questionnaire -> 'answers' -> 'owner' ->> 'owner-id' = $2`,
+                `SELECT id, created, modified, expires, submission_status, questionnaire ->> 'version' AS "questionnaire_version" FROM questionnaire WHERE id = $1 AND questionnaire -> 'answers' -> 'owner' ->> 'owner-id' = $2`,
                 [questionnaireId, ownerId]
             );
         } catch (err) {
