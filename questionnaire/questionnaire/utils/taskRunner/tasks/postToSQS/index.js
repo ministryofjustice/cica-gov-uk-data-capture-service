c0f7c62bd3fe3e354f317b3ca711a5c1c143b596
neil-stephen-mcgonigle <Neil.Mcgonigle@CICA.GOV.UK>
2023-11-24 08:54:47 +0000

    Fix send message to application form sqs queue error handling

--- questionnaire/questionnaire/utils/taskRunner/tasks/postToSQS/index.js
+++ questionnaire/questionnaire/utils/taskRunner/tasks/postToSQS/index.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const VError = require('verror');
 const createSqsService = require('../../../../../../services/sqs');
 
 /**
@@ -24,24 +23,8 @@ async function sendSubmissionMessageToSQS({questionnaire, logger}) {
     const body = buildMessageBody(questionnaireId);
     logger.info(`Sending submission message to SQS for questionnaire with id ${questionnaireId}`);
     const sqsService = createSqsService({logger});
-    const sqsResponse = await sqsService.send(body, process.env.AWS_SQS_ID);
 
-    logger.info(sqsResponse);
-
-    if (!sqsResponse || !sqsResponse.MessageId) {
-        logger.error(
-            `Message not sent to SQS for questionnaire with id ${questionnaireId} with error ${sqsResponse}`
-        );
-        throw new VError(
-            {
-                name: 'SendMessageFailed'
-            },
-            `Message not sent to SQS for questionnaire with id ${questionnaireId}`
-        );
-    }
-
-    // return something
-    return sqsResponse;
+    return sqsService.send(body, process.env.AWS_SQS_ID);
 }
 
 module.exports = {
