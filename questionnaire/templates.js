b527a05d16226b42f98ee9660946fd0ecd7bcf12
tjbburton <Thomas.Burton@CICA.gov.uk>
2024-05-24 10:50:12 +0100

    Add support for action templates

--- questionnaire/templates.js
+++ questionnaire/templates.js
@@ -2,15 +2,37 @@
 
 const applicationTemplate = require('q-templates-application');
 
+const letterTemplates = require('q-templates-actions');
+
 const applicationTemplateAsJson = JSON.stringify(applicationTemplate);
+
+const tcqeTemplateAsJson = JSON.stringify(letterTemplates.tcqe);
+
+const decisionLetterTemplateAsJson = JSON.stringify(letterTemplates.decisionletter);
 
 function getApplicationTemplateCopy() {
     return JSON.parse(applicationTemplateAsJson);
+}
+
+function getTcqeTemplateCopy() {
+    return JSON.parse(tcqeTemplateAsJson);
+}
+
+function getDecisionLetterTemplateCopy() {
+    return JSON.parse(decisionLetterTemplateAsJson);
 }
 
 module.exports = {
     'sexual-assault': id => ({
         id,
         ...getApplicationTemplateCopy()
+    }),
+    tcqe: id => ({
+        id,
+        ...getTcqeTemplateCopy()
+    }),
+    decisionletter: id => ({
+        id,
+        ...getDecisionLetterTemplateCopy()
     })
 };
