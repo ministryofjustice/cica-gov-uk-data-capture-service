19064b88873b4458055639b2b851f1f9036294fb
tjbburton <Thomas.Burton@CICA.gov.uk>
2024-09-04 12:13:50 +0100

    Get page type meta data

--- questionnaire/questionnaire-service.js
+++ questionnaire/questionnaire-service.js
@@ -350,6 +350,7 @@ function createQuestionnaireService({
         return {
             summary: questionnaire.routes.summary,
             confirmation: questionnaire.routes.confirmation,
+            pageType: questionnaire.sections[sectionId]?.schema?.meta?.pageType,
             final: isFinalType
         };
     }
